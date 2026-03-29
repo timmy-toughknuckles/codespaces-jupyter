4c9bb5d830e7b495b0209b244574da02127d3aef
timmy-toughknuckles <austin.gu2022@gmail.com>
2026-03-29 23:29:05 +0000

    made calculator 1.0

--- calc.py
+++ calc.py
@@ -1,4 +1,29 @@
-def calc(n1, n2):
-    return n1 + n2
+print('Hello, I am your personal calculator!')
+print('What would you like me to do?')
 
-print(calc(1,2))
+def bob_is_sigma(expr):
+    if '+' in expr:
+        a = expr.split('+')
+        print(int(a[0])+int(a[1]))
+    elif '-' in expr:
+        b = expr.split('-')
+        print(int(b[0])-int(b[1]))
+    elif '*' in expr:
+        c = expr.split('*')
+        print(int(c[0])*int(c[1]))
+    elif '/' in expr:
+        d = expr.split('/')
+        print(int(c[0])/int(c[1]))
+
+
+
+while True:
+    result1=input('')
+    if result1=='':
+        print('Invalid Operation!')
+    
+    elif result1!='':
+        if result1=='reset' or result1=='r':
+            break
+        else:
+            bob_is_sigma(result1)
