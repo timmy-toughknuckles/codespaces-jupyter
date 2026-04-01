0e945b525e9d6b4dccfa707d8e9aaf19bc06b23a
timmy-toughknuckles <austin.gu2022@gmail.com>
2026-04-01 00:42:55 +0000

    Add option to skip asking for input again in calculator

--- calculator2.py
+++ calculator2.py
@@ -12,7 +12,30 @@ elif operation == '/':
 
 while True:
     c = input('If you want, you can type "reset" or "r" to reset the calculator: ')
+    if c == "Don't ask me again":
+        while True:
+            a = input('Enter your first number: ')
+            b = input('Enter your second number: ')
+            operation = input('Enter the operation you want to perform (e.g. +, -, *, /): ')
+            if operation == '+':
+                print(float(a) + float(b))
+            elif operation == '-':
+                print(float(a) - float(b))
+            elif operation == '*':
+                print(float(a) * float(b))
+            elif operation == '/':
+                print(float(a) / float(b))
     if c == 'reset' or c == 'r':
         break
-    elif c != 'reset' and c != 'r':
-        print('Invalid Operation!')
+    if c == 'no':
+        a = input('Enter your first number: ')
+        b = input('Enter your second number: ')
+        operation = input('Enter the operation you want to perform (e.g. +, -,  *, /): ')
+        if operation == '+':
+            print(float(a) + float(b))
+        elif operation == '-':
+            print(float(a) - float(b))
+        elif operation == '*':
+            print(float(a) * float(b))
+        elif operation == '/':
+            print(float(a) / float(b))
