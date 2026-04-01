66bda6b437b0c506c864a59007fb61ec62ea9a60
timmy-toughknuckles <austin.gu2022@gmail.com>
2026-04-01 00:43:09 +0000

    Refactor calculator logic into a function for better code organization

--- calculator2.py
+++ calculator2.py
@@ -1,41 +1,31 @@
-a = input('Enter your first number: ')
-b = input('Enter your second number: ')
-operation = input('Enter the operation you want to perform (e.g. +, -, *, /): ')
-if operation == '+':
-    print(float(a) + float(b))
-elif operation == '-':
-    print(float(a) - float(b))
-elif operation == '*':
-    print(float(a) * float(b))
-elif operation == '/':
-    print(float(a) / float(b))
+def calculator():
+    a = input('Enter your first number: ')
+    b = input('Enter your second number: ')
+    operation = input('Enter the operation you want to perform (e.g. +, -, *, /): ')
+    if operation == '+':
+        print(float(a) + float(b))
+    if operation == '-':
+        print(float(a) - float(b))
+    if operation == '*':
+        print(float(a) * float(b))
+    if operation == '/':
+        if float(b) == 0:
+            print("Error: Division by zero is not allowed.")
+        else:
+            print(float(a) / float(b))
+
+
+
+
+calculator()
 
 while True:
     c = input('If you want, you can type "reset" or "r" to reset the calculator: ')
     if c == "Don't ask me again":
         while True:
-            a = input('Enter your first number: ')
-            b = input('Enter your second number: ')
-            operation = input('Enter the operation you want to perform (e.g. +, -, *, /): ')
-            if operation == '+':
-                print(float(a) + float(b))
-            elif operation == '-':
-                print(float(a) - float(b))
-            elif operation == '*':
-                print(float(a) * float(b))
-            elif operation == '/':
-                print(float(a) / float(b))
+            calculator()
     if c == 'reset' or c == 'r':
         break
     if c == 'no':
-        a = input('Enter your first number: ')
-        b = input('Enter your second number: ')
-        operation = input('Enter the operation you want to perform (e.g. +, -,  *, /): ')
-        if operation == '+':
-            print(float(a) + float(b))
-        elif operation == '-':
-            print(float(a) - float(b))
-        elif operation == '*':
-            print(float(a) * float(b))
-        elif operation == '/':
-            print(float(a) / float(b))
+        calculator()
+        
